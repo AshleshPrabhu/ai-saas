782a2374c2e5d5ccfa7c9a94347d76cdb1c84e48
Ashlesh Prabhu <ashlesh.prabhu5@gmail.com>
2024-12-26 13:35:23 +0530

    refactor: simplify download link creation in video handling

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -34,18 +34,14 @@ function Home() {
     fetchVideos()
   }, [fetchVideos])
   const handleDownload = useCallback((url: string, title: string) => {
-    () => {
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", `${title}.mp4`);
-        link.setAttribute("target", "_blank");
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-
-    }
-
-  }, [])
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", `${title}.mp4`);
+    link.setAttribute("target", "_blank");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }, []);
 
   if(loading){
       return <div>Loading...</div>
